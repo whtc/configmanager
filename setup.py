466a0d8d4b9a8dcc01d821eb36d7f88eb1477d6e
Hugh T. Ranalli <hugh@whtc.ca>
2014-11-25 10:17:53 -0500

    Initial commit, release 1.0rc1 Licensed under GPLv3

---
+++ setup.py
@@ -0,0 +1,69 @@
+import os 
+from setuptools import (
+    setup, 
+    find_packages
+    )
+
+name="whtc.recipe.configmanager"
+version = '1.0rc1'
+tests_require=[
+    'zope.testing', 
+    'zc.buildout',
+    'collective.recipe.template', 
+    ]
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name=name,
+    keywords='config configuration recipe buildout template',
+    version=version,
+    description=(
+        "A buildout recipe to manage auto-populating portions of shared "
+        "configuration files that cannot simply be overwritten."
+        ),
+    long_description=(
+        read('README.txt')
+        + '\n' +
+        read('whtc', 'recipe', 'configmanager', 'README.txt')
+        + '\n' +
+        read('CHANGES.txt')
+        + '\n' +
+        'Download\n'
+        '********\n'
+        ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Buildout :: Recipe",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Topic :: System :: Software Distribution",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+    author='Hugh T. Ranalli',
+    author_email='hugh@whtc.ca',
+    url='http://pypi.python.org/pypi/whtc.recipe.configmanager',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['whtc', 'whtc.recipe' ],
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'setuptools',
+        'zc.buildout',
+        'collective.recipe.template',
+        ],
+    tests_require=tests_require,
+    extras_require={
+        'test' : tests_require,
+        'templates' : ['collective.recipe.template', ],
+        },
+    
+    test_suite='%s.tests.test_suite' % name,
+    entry_points={
+        'zc.buildout': ['default = %s:Recipe' % name],
+        'zc.buildout.uninstall': ['default = %s:uninstall' % name],
+        },
+      )
---
+++ whtc/recipe/configmanager/tests.py
@@ -0,0 +1,71 @@
+"""
+Run whtc.recipe.configmanager doc tests
+"""
+__docformat__ = 'restructuredtext'
+
+import errno
+import os
+import unittest
+
+import zc.buildout.testing
+
+from zope.testing import doctest, renormalizing
+
+import collective.recipe.template
+
+optionflags =  (doctest.ELLIPSIS |
+                doctest.NORMALIZE_WHITESPACE |
+                doctest.REPORT_ONLY_FIRST_FAILURE)
+
+def _remove_test_files(test):
+    test_path = os.path.join(os.path.dirname(test.filename), 'testdata')
+    for name in [
+        'OUTPUT.TXT', 
+        'TEST_FILE.INI', 
+        'TEST_FILE.INI.BK0',
+        'TEST_FILE.INI.BK1',
+        ]:
+            file_path = os.path.join(test_path, name)
+            try:
+                if os.stat(file_path):
+                    os.remove(file_path)
+            except OSError, err:
+                if err.errno != errno.ENOENT:
+                    raise
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install('collective.recipe.template', test)
+    zc.buildout.testing.install_develop('whtc.recipe.configmanager', test)
+
+    # Clean up any leftover files...
+    _remove_test_files(test)
+    
+def tearDown(test):
+    # Clean up any leftover files...
+    _remove_test_files(test)
+
+    zc.buildout.testing.buildoutTearDown(test)
+
+def test_suite():
+    suite = unittest.TestSuite((
+            doctest.DocFileSuite(
+                'README.txt',
+                setUp=setUp,
+                tearDown=tearDown,
+                optionflags=optionflags,
+                checker=renormalizing.RENormalizing([
+                        # If want to clean up the doctest output you
+                        # can register additional regexp normalizers
+                        # here. The format is a two-tuple with the RE
+                        # as the first item and the replacement as the
+                        # second item, e.g.
+                        # (re.compile('my-[rR]eg[eE]ps'), 'my-regexps')
+                        zc.buildout.testing.normalize_path,
+                        ]),
+                ),
+            ))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
